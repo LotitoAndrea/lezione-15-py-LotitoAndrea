19d9307b393654f0a4e29874b9681de9dcf7bf99
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-03-11 12:00:36 +0100

    da finire

---
+++ 01_DizionarioPersona.py
@@ -0,0 +1,11 @@
+persona = {"nome": "Marco", "età": 17, "città": "Torino"}
+print(persona["nome"])
+print(persona["età"])
+print(persona["città"])
+# print(persona["professione"])
+
+dati = {"nome": "Luca", "età": 30, "città": "Roma"}
+print(dati.keys())
+print(dati.values())
+print(dati.items())
+
