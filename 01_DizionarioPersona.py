27bb8a4e94b73f696ab001aa245fc2a9bdfd911f
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-03-12 12:26:30 +0100

    da finire

--- 01_DizionarioPersona.py
+++ 01_DizionarioPersona.py
@@ -1,11 +1,20 @@
+# parte 1
 persona = {"nome": "Marco", "età": 17, "città": "Torino"}
 print(persona["nome"])
 print(persona["età"])
 print(persona["città"])
 # print(persona["professione"])
 
+# parte 2
 dati = {"nome": "Luca", "età": 30, "città": "Roma"}
 print(dati.keys())
 print(dati.values())
 print(dati.items())
 
+# parte 3
+dizionario = {"a": 10, "b": 20, "c": 30}
+chiave = "b"
+if chiave in dizionario:
+    print(f"La chiave '{chiave}' è presente nel dizionario con valore {dizionario.get(chiave)}")
+else:
+    print(f"Chiave non trovata, {dizionario.get(chiave)}")
