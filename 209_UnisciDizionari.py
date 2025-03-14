1603fc71d92688706687e898a1c4f7bae5a1e929
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2025-03-14 12:01:06 +0100

    yippie gabry thanks

--- 209_UnisciDizionari.py
+++ 209_UnisciDizionari.py
@@ -1,8 +1,9 @@
-persona = {"nome": "Marco", "età": 19, "città": "Torino"}
-prodotto = {"prodotto": "Latte", "prezzo": 1.20, "quantità": 2}
-animale = {"animale": "Cane", "razza": "Pastore Tedesco", "età": 3}
+dizionario1 = {"nome": "Marco", "età": 19, "città": "Torino"}
+dizionario2 = {"nome": "Giovanni il distruttore di Galassie e automaton !!!", "razza": "Pastore Tedesco", "età": 3}
 
-unione = {**persona, **prodotto, **animale}
-print(unione)
-
-# da finire
+for i in dizionario2:
+    if type(dizionario2[i]) == int:
+        dizionario1[i] = dizionario1.get(i, 0), dizionario2[i]
+    if type(dizionario2[i]) == str:
+        dizionario1[i] = dizionario1.get(i, ""), dizionario2[i]
+print(dizionario1)
